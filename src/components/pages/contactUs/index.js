7d8691883972346d16ef36b4abe114188443f090
khadije <khadijeh.allahgholi@gmail.com>
2022-12-22 22:15:04 +0330

    form added to aboutUs page

--- src/components/pages/contactUs/index.js
+++ src/components/pages/contactUs/index.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import { Button, Form } from "react-bootstrap";
+import style from "./contact.module.css";
 
 function ContactUs() {
   return (
-    <div>
-      
+    <div className={style.contact}>
+      <h2>تماس با ما</h2>
+      <Form className={style.form}>
+        <Form.Group className="mb-3">
+          <Form.Control type="text" placeholder="نام و نام خانوادگی" size="sm" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control type="email" placeholder="آدرس ایمیل" size="sm"/>
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control as="textarea" placeholder="پیام" rows={3} size="sm"/>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+        ثبت
+      </Button>
+      </Form>
     </div>
-  )
+  );
 }
 
-export default ContactUs
+export default ContactUs;
