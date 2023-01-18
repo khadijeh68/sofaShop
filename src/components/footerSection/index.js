24054a3ba17851ccd0177370700720fe9a6640cf
khadije <khadijeh.allahgholi@gmail.com>
2023-01-18 21:31:55 +0330

    UI:footerSection component added

--- src/components/footerSection/index.js
+++ src/components/footerSection/index.js
@@ -7,6 +7,44 @@ import samandehiLogo from "../../assets/images/namads/samandehiLogo.png";
 const FooterSection = () => {
   return (
     <div className={styles.main}>
+      <div className={styles.shoppingGuide}>
+        <ul>
+          <p>راهنمای خرید</p>
+          <li>
+            <a href="/">نحوه ثبت سفارش</a>
+          </li>
+          <li>
+            <a href="/">رویه ارسال سفارش</a>
+          </li>
+          <li>
+            <a href="/">شیوه های ارسال سفارش</a>
+          </li>
+          <li>
+            <a href="/">شیوه های پرداخت</a>
+          </li>
+          <li>
+            <a href="/">فروش اقساطی</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className={styles.account}>
+        <ul>
+          <p>خدمات مشتریان</p>
+          <li>
+            <a href="/">پاسخ به پرسش های متداول</a>
+          </li>
+          <li>
+            <a href="/">شرایط استفاده</a>
+          </li>
+        </ul>
+      </div>
+
+      <form action="">
+        <p>با ثبت ایمیل، از آخرین محصولات و جدیدترین تخفیف ها باخبر شوید</p>
+        <input className={styles.email} type="text" placeholder="ایمیل"/>
+        <button className={styles.email_btn}>ثبت</button>
+      </form>
       <div className={styles.namads}>
         <div>
           <img src={anjomanLogo} alt="anjoman Logo" />
